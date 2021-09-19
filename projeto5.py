f162854ad0e0f21512ea14d6ae9ebb89782cf404
SnooWStriKeer1 <67876978+SnooWStriKeer1@users.noreply.github.com>
2021-09-19 08:13:57 -0300

    Delete projeto5.py

--- projeto5.py
+++
@@ -1,5 +0,0 @@
-n1 =float(input('digite a primeira nota'))
-n2 =float(input('digite a segunda nota'))
-m =(n1+n2)/2
-print ('A sua media foi {}'.format(m))
-print ('Parabens' if m >=6 else 'ESTUDO MAIS SEU BURRO')
